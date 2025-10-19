1f20cb734cd5fc6cb2b6b2d7db29a3e65ea29147
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-10-19 09:21:01 +0530

    CSES Problem Solution

---
+++ CSES_Problem.cpp
@@ -0,0 +1,46 @@
+/*
+Time limit: 1.00 s
+Memory limit: 512 MB
+
+Given an array of n integers, your task is to find the maximum sum of values in a contiguous, nonempty subarray.
+Input
+The first input line has an integer n: the size of the array.
+The second line has n integers x_1,x_2,\dots,x_n: the array values.
+Output
+Print one integer: the maximum subarray sum.
+Constraints
+
+1 \le n \le 2 \cdot 10^5
+-10^9 \le x_i \le 10^9
+*/
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+ll maximum_subarray_sum(ll n, ll *array)
+{
+    ll best = array[0], current_sum = array[0];
+    for (ll i = 1; i < n; i++)
+    {
+        current_sum = max(array[i], current_sum + array[i]);
+        best = max(best, current_sum);
+    }
+    return best;
+}
+
+int main()
+{
+    ll size;
+    cin >> size;
+
+    ll *arr = new ll[size];
+    for (ll i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << maximum_subarray_sum(size, arr);
+    delete[] arr;
+
+    return 0;
+}
