cdf2803bb1ec273bdc3f6c1da36e4c50b42a3b63
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-10-31 16:42:23 +0530

    merge_sort

---
+++ Sorting Algorithms/merge_sort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+void merge(vector<int> &array, int st, int mid, int end)
+{
+    vector<int> temp;
+    int i = st, j = mid + 1;
+
+    while (i <= mid && j <= end)
+    {
+        if (array[i] < array[j])
+        {
+            temp.push_back(array[i]);
+            i++;
+        }
+
+        else
+        {
+            temp.push_back(array[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp.push_back(array[i]);
+        i++;
+    }
+
+    while (j <= end)
+    {
+        temp.push_back(array[j]);
+        j++;
+    }
+
+    for (int i = 0; i < temp.size(); i++)
+    {
+        array[i + st] = temp[i];
+    }
+}
+
+void mergeSort(vector<int> &array, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + ((end - st) / 2);
+
+        mergeSort(array, st, mid);
+        mergeSort(array, mid + 1, end);
+
+        merge(array, st, mid, end);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {12, 35, 35, 8, 32, 17};
+
+    mergeSort(arr, 0, arr.size() - 1);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
